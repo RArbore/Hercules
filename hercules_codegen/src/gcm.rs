a27afc8959c9820978ae4058af66cd1f14f7eef0
Russel Arbore <russel.jma@gmail.com>
2023-10-23 10:57:32 -0500

    fix

--- hercules_codegen/src/gcm.rs
+++ hercules_codegen/src/gcm.rs
@@ -16,7 +16,6 @@ pub fn gcm(
 ) -> Vec<NodeID> {
     // Step 1: find the immediate control uses and immediate control users of
     // each node.
-
     let immediate_control_uses =
         forward_dataflow(function, reverse_postorder, |inputs, node_id| {
             immediate_control_flow(inputs, node_id, function)
@@ -25,21 +24,6 @@ pub fn gcm(
         backward_dataflow(function, def_use, reverse_postorder, |inputs, node_id| {
             immediate_control_flow(inputs, node_id, function)
         });
-
-    for i in 0..function.nodes.len() {
-        for j in 0..function.nodes.len() {
-            if immediate_control_uses[i].is_set(NodeID::new(j)) {
-                println!("Node {} uses node {}", i, j);
-            }
-        }
-
-        for j in 0..function.nodes.len() {
-            if immediate_control_users[i].is_set(NodeID::new(j)) {
-                println!("Node {} users node {}", i, j);
-            }
-        }
-        println!("");
-    }
 
     todo!()
 }
