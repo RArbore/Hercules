c9e579701648fd160836b68bebe539389099fe9b
Russel Arbore <russel.jma@gmail.com>
2023-11-20 18:04:57 -0800

    Won't be needing anti dependence edges for product types

--- hercules_codegen/src/antideps.rs
+++ hercules_codegen/src/antideps.rs
@@ -17,36 +17,8 @@ pub fn antideps(function: &Function, def_use: &ImmutableDefUseMap) -> Vec<(NodeI
     let mut antideps = vec![];
 
     for id in (0..function.nodes.len()).map(NodeID::new) {
+        // We only need to consider array reads and writes.
         let users = def_use.get_users(id);
-
-        // First, handle product reads and writes.
-        let reads = users.iter().filter(|user| {
-            if let Node::ReadProd { prod, index: _ } = function.nodes[user.idx()] {
-                prod == id
-            } else {
-                false
-            }
-        });
-        let mut writes = users.iter().filter(|user| {
-            if let Node::WriteProd {
-                prod,
-                index: _,
-                data: _,
-            } = function.nodes[user.idx()]
-            {
-                prod == id
-            } else {
-                false
-            }
-        });
-        if let Some(write) = writes.next() {
-            for read in reads {
-                antideps.push((*read, *write));
-            }
-        }
-        assert!(writes.next() == None, "Can't form anti-dependencies when there are two independent writes depending on a single product value.");
-
-        // Second, handle array reads and writes.
         let reads = users.iter().filter(|user| {
             if let Node::ReadArray { array, index: _ } = function.nodes[user.idx()] {
                 array == id
@@ -66,6 +38,8 @@ pub fn antideps(function: &Function, def_use: &ImmutableDefUseMap) -> Vec<(NodeI
                 false
             }
         });
+
+        // If there are any writes, compute the anti dependence edges.
         if let Some(write) = writes.next() {
             for read in reads {
                 antideps.push((*read, *write));
