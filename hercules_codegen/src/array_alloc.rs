c36abddc8f6a9b55b04aa8c12fe68f08aca6e9ba
Russel Arbore <russel.jma@gmail.com>
2023-12-02 15:48:20 -0600

    Define what logical array allocation is

--- hercules_codegen/src/array_alloc.rs
+++ hercules_codegen/src/array_alloc.rs
@@ -1,12 +1,19 @@
 extern crate hercules_ir;
 
-use self::hercules_ir::dataflow::*;
+use std::collections::HashMap;
+
 use self::hercules_ir::ir::*;
 
-use self::ArrayAllocSemilattice::*;
-
 /*
- * Enum for array allocation semilattice.
+ * Top level function to allocate individual arrays for sets of nodes in the IR
+ * graph. Doesn't attempt to overlap allocations w/ liveness analysis. Returns
+ * a set of nodes per allocation, which are the nodes that use that allocation,
+ * along with dimensions specified with dynamic constants.
  */
-#[derive(PartialEq, Eq, Clone, Debug)]
-enum ArrayAllocSemilattice {}
+pub fn logical_array_alloc(
+    function: &Function,
+    fork_join_map: &HashMap<NodeID, NodeID>,
+    bbs: &Vec<Vec<NodeID>>,
+    fork_join_nests: &HashMap<NodeID, Vec<NodeID>>,
+) -> () {
+}
