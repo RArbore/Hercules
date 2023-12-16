f9267eac789e1a32b3e463a82253981437ff2da3
Russel Arbore <russel.jma@gmail.com>
2023-12-15 18:02:26 -0800

    Finish logical array alloc

--- hercules_codegen/src/array_alloc.rs
+++ hercules_codegen/src/array_alloc.rs
@@ -31,7 +31,10 @@ impl UnifyKey for UnitKey {
  * Top level function to allocate individual arrays for sets of nodes in the IR
  * graph. Doesn't attempt to overlap allocations w/ liveness analysis. Returns
  * a set of nodes per allocation, which are the nodes that use that allocation,
- * along with dimensions specified with dynamic constants.
+ * along with dimensions specified with dynamic constants. The return value
+ * consists of a list of array allocations (described by their dynamic constant
+ * size), and a map from nodes involving array values to the number of the array
+ * allocation they operate on.
  */
 pub fn logical_array_alloc(
     function: &Function,
@@ -40,7 +43,7 @@ pub fn logical_array_alloc(
     fork_join_map: &HashMap<NodeID, NodeID>,
     bbs: &Vec<NodeID>,
     fork_join_nests: &HashMap<NodeID, Vec<NodeID>>,
-) -> () {
+) -> (Vec<Vec<DynamicConstantID>>, HashMap<NodeID, usize>) {
     // Step 1: filter nodes that operate on arrays, either on their input or
     // their output.
     let id_outputs_array = |id: &NodeID| types[typing[id.idx()].idx()].is_array();
@@ -93,8 +96,8 @@ pub fn logical_array_alloc(
     // WriteArray: provides the base array dimensions, in addition to dimensions
     // corresponding to each fork / join the node is nested in
     let mut key_to_value_size: HashMap<UnitKey, Vec<DynamicConstantID>> = HashMap::new();
-    for key in keys {
-        let value_key = allocs.find(key);
+    for key in keys.iter() {
+        let value_key = allocs.find(*key);
         let id = array_nodes[key.index() as usize];
 
         let extents = match function.nodes[id.idx()] {
@@ -140,10 +143,12 @@ pub fn logical_array_alloc(
         };
 
         // The largest required size is the correct size. It is assumed that all
-        // sizes calculated above form a total order with respect to sub
-        // vectoring. That is, no two vectors calculated above for the same
-        // array value will contain differing elements at any given position.
+        // sizes calculated above form a total order with respect to suffix
+        // vectoring.
         if let Some(old_extents) = key_to_value_size.get(&value_key) {
+            assert!(
+                std::iter::zip(old_extents.iter().rev(), extents.iter().rev()).all(|(a, b)| a == b)
+            );
             if old_extents.len() < extents.len() {
                 key_to_value_size.insert(value_key, extents);
             }
@@ -151,7 +156,24 @@ pub fn logical_array_alloc(
             key_to_value_size.insert(value_key, extents);
         }
     }
-    println!("{:?}", key_to_value_size);
+
+    // Step 4: collect array allocations as a numbered list. Map from array
+    // nodes to the array allocation number they use.
+    let mut logical_allocations: Vec<Vec<DynamicConstantID>> = vec![];
+    let mut key_to_number: HashMap<UnitKey, usize> = HashMap::new();
+    for (key, array_value) in key_to_value_size {
+        key_to_number.insert(key, logical_allocations.len());
+        logical_allocations.push(array_value);
+    }
+    let mut node_to_logical_numbers: HashMap<NodeID, usize> = HashMap::new();
+    for node in array_nodes.iter() {
+        node_to_logical_numbers.insert(
+            *node,
+            key_to_number[&allocs.find(keys[array_node_numbers[node]])],
+        );
+    }
+
+    (logical_allocations, node_to_logical_numbers)
 }
 
 /*
