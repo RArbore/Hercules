9d925bc4d1c64fe02f3629266c47bc2a94ffe58e
Russel Arbore <russel.jma@gmail.com>
2024-01-28 17:55:26 -0600

    Update antideps

--- hercules_codegen/src/antideps.rs
+++ hercules_codegen/src/antideps.rs
@@ -9,31 +9,35 @@ use self::hercules_ir::ir::*;
  * the write node.
  */
 pub fn antideps(function: &Function, def_use: &ImmutableDefUseMap) -> Vec<(NodeID, NodeID)> {
-    // Array typed values are not directly computed on. Thus, there are actually
-    // very few nodes that have array inputs or output. As a result, when
-    // forming anti-dependencies for a single allocation, we only need to
+    // Collection values are not directly computed on. Thus, there are actually
+    // very few nodes that have collection inputs or output. As a result, when
+    // forming anti-dependencies for a single collection, we only need to
     // consider immediate users that are read or write nodes - no proper
     // dataflow analysis necessary.
     let mut antideps = vec![];
 
     for id in (0..function.nodes.len()).map(NodeID::new) {
-        // We only need to consider array reads and writes.
+        // Collect the reads and writes to / from this collection.
         let users = def_use.get_users(id);
         let reads = users.iter().filter(|user| {
-            if let Node::ReadArray { array, index: _ } = function.nodes[user.idx()] {
-                array == id
+            if let Node::Read {
+                collect,
+                indices: _,
+            } = function.nodes[user.idx()]
+            {
+                collect == id
             } else {
                 false
             }
         });
         let mut writes = users.iter().filter(|user| {
-            if let Node::WriteArray {
-                array,
-                index: _,
+            if let Node::Write {
+                collect,
                 data: _,
+                indices: _,
             } = function.nodes[user.idx()]
             {
-                array == id
+                collect == id
             } else {
                 false
             }
@@ -45,7 +49,9 @@ pub fn antideps(function: &Function, def_use: &ImmutableDefUseMap) -> Vec<(NodeI
                 antideps.push((*read, *write));
             }
         }
-        assert!(writes.next() == None, "Can't form anti-dependencies when there are two independent writes depending on a single array value.");
+
+        // TODO: Multiple write uses should clone to collection for N - 1 of the writes.
+        assert!(writes.next() == None, "Can't form anti-dependencies when there are two independent writes depending on a single collection value.");
     }
 
     antideps
