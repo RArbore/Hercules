7cd16d6b278df17231b6b3a79d8643099ac840eb
Russel Arbore <russel.jma@gmail.com>
2024-02-06 18:29:50 -0600

    Some elf parsing

--- hercules_rt/src/exec.rs
+++ hercules_rt/src/exec.rs
@@ -1,10 +1,12 @@
+use std::ffi::CString;
+use std::os::raw::c_char;
 use std::path::Path;
 
 extern "C" {
-    pub fn load_elf();
+    pub fn load_elf(path: *const c_char);
 }
 
 pub fn load_binary(path: &Path) {
-    println!("load_binary: {:?}", path);
-    unsafe { load_elf() };
+    let path_cstring = CString::new(path.as_os_str().as_encoded_bytes()).unwrap();
+    unsafe { load_elf(path_cstring.as_ptr()) };
 }
--- hercules_rt/src/load.c
+++ hercules_rt/src/load.c
@@ -1,5 +1,58 @@
+#include <sys/types.h>
+#include <sys/mman.h>
+#include <sys/stat.h>
+#include <assert.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <errno.h>
+#include <fcntl.h>
 #include <stdio.h>
+#include <elf.h>
 
-void load_elf(void) {
-    printf("load_elf\n");
+/*
+ * This function is used for loading ELF object files. The goal is to take the
+ * machine code for a Hercules function and invoke it in the runtime. This
+ * function returns a structure containing heap allocated data, which needs to
+ * be deallocated using free_elf.
+ */
+void load_elf(const char *path) {
+    printf("load_elf: %s\n", path);
+
+    int fd = open(path, O_RDONLY);
+    if (fd == -1) {
+	fprintf(stderr, "ERROR: Couldn't open file at %s.\n", path);
+	exit(1);
+    }
+
+    struct stat sb;
+    if (fstat(fd, &sb) == -1) {
+        fprintf(stderr, "ERROR: Couldn't fstat the file at %s.\n", path);
+        exit(1);
+    }
+
+    char *base = mmap(NULL, sb.st_size, PROT_READ, MAP_PRIVATE, fd, 0);
+    if (base == MAP_FAILED) {
+        fprintf(stderr, "ERROR, Couldn't map the file at %s.\n", path);
+        exit(1);
+    }
+
+    Elf64_Ehdr header;
+    memcpy(&header, base, sizeof(header));
+
+    assert(header.e_shentsize == sizeof(Elf64_Shdr));
+    Elf64_Shdr section_header_table[header.e_shnum];
+    memcpy(section_header_table, base + header.e_shoff, header.e_shnum * sizeof(Elf64_Shdr));
+
+    char *shstrtab = base + section_header_table[header.e_shstrndx].sh_offset;
+    for (int i = 0; i < header.e_shnum; ++i) {
+	printf("%s\n", shstrtab + section_header_table[i].sh_name);
+    }
+
+    if (munmap(base, sb.st_size) == -1) {
+	fprintf(stderr, "ERROR: Couldn't unmap the file at %s.\n", path);
+	exit(1);
+    }
+
+    close(fd);
 }
