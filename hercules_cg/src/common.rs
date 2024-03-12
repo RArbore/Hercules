0e32fceeab397045e8f515085b3e2ea52623ce07
Russel Arbore <russel.jma@gmail.com>
2024-03-11 19:36:25 -0600

    Refactor type, cons, and dyn_cons lowering

--- hercules_cg/src/common.rs
+++ hercules_cg/src/common.rs
@@ -119,3 +119,149 @@ pub(crate) fn emit_function_signature<W: Write>(
 ) -> std::fmt::Result {
     todo!()
 }
+
+/*
+ * Types, constants, and dynamic constants are fairly simple to translate into
+ * LLVM IR.
+ */
+
+pub(crate) fn generate_type_string(ty: &Type, llvm_types: &Vec<String>) -> String {
+    match ty {
+        Type::Control(_) => {
+            // Later, we create virtual registers corresponding to fork nodes of
+            // type i64, so we need the "type" of the fork node to be i64.
+            "i64".to_string()
+        }
+        Type::Boolean => "i1".to_string(),
+        Type::Integer8 | Type::UnsignedInteger8 => "i8".to_string(),
+        Type::Integer16 | Type::UnsignedInteger16 => "i16".to_string(),
+        Type::Integer32 | Type::UnsignedInteger32 => "i32".to_string(),
+        Type::Integer64 | Type::UnsignedInteger64 => "i64".to_string(),
+        Type::Float32 => "float".to_string(),
+        Type::Float64 => "double".to_string(),
+        // Because we traverse in bottom-up order, we can assume that the LLVM
+        // types for children types are already computed.
+        Type::Product(fields) => {
+            let mut iter = fields.iter();
+            if let Some(first) = iter.next() {
+                iter.fold("{".to_string() + &llvm_types[first.idx()], |s, f| {
+                    s + ", " + &llvm_types[f.idx()]
+                }) + "}"
+            } else {
+                "{}".to_string()
+            }
+        }
+        Type::Array(_, _) => {
+            // Array types becomes pointers. The element type and dynamic
+            // constant bounds characterize the access code we generate later,
+            // not the type itself.
+            "ptr".to_string()
+        }
+        Type::Summation(_) => todo!(),
+    }
+}
+
+pub(crate) fn generate_type_strings(module: &Module) -> Vec<String> {
+    // Render types into LLVM IR. This requires translating from our interning
+    // structures to LLVM types. We can't just blow through the types vector,
+    // since a type may reference a type ID ahead of it in the vector. Instead,
+    // iterate types in a bottom up order with respect to the type intern DAGs.
+    let mut llvm_types = vec!["".to_string(); module.types.len()];
+    for id in module.types_bottom_up() {
+        llvm_types[id.idx()] = generate_type_string(&module.types[id.idx()], &llvm_types);
+    }
+
+    llvm_types
+}
+
+pub(crate) fn generate_constant_string(
+    cons_id: ConstantID,
+    cons: &Constant,
+    tys: &Vec<Type>,
+    llvm_constants: &Vec<String>,
+) -> String {
+    match cons {
+        Constant::Boolean(val) => {
+            if *val {
+                "true".to_string()
+            } else {
+                "false".to_string()
+            }
+        }
+        Constant::Integer8(val) => format!("{}", val),
+        Constant::Integer16(val) => format!("{}", val),
+        Constant::Integer32(val) => format!("{}", val),
+        Constant::Integer64(val) => format!("{}", val),
+        Constant::UnsignedInteger8(val) => format!("{}", val),
+        Constant::UnsignedInteger16(val) => format!("{}", val),
+        Constant::UnsignedInteger32(val) => format!("{}", val),
+        Constant::UnsignedInteger64(val) => format!("{}", val),
+        Constant::Float32(val) => {
+            if val.fract() == 0.0 {
+                format!("{}.0", val)
+            } else {
+                format!("{}", val)
+            }
+        }
+        Constant::Float64(val) => {
+            if val.fract() == 0.0 {
+                format!("{}.0", val)
+            } else {
+                format!("{}", val)
+            }
+        }
+        Constant::Product(_, fields) => {
+            let mut iter = fields.iter();
+            if let Some(first) = iter.next() {
+                iter.fold("{".to_string() + &llvm_constants[first.idx()], |s, f| {
+                    s + ", " + &llvm_constants[f.idx()]
+                }) + "}"
+            } else {
+                "{}".to_string()
+            }
+        }
+        Constant::Array(_, _) => format!("%arr.{}", cons_id.idx()),
+        Constant::Summation(_, _, _) => todo!(),
+        Constant::Zero(ty_id) => render_zero_constant(cons_id, &tys[ty_id.idx()]),
+    }
+}
+
+pub(crate) fn generate_constant_strings(module: &Module) -> Vec<String> {
+    // Render constants into LLVM IR. This is done in a very similar manner as
+    // types.
+    let mut llvm_constants = vec!["".to_string(); module.constants.len()];
+    for id in module.constants_bottom_up() {
+        llvm_constants[id.idx()] = generate_constant_string(
+            id,
+            &module.constants[id.idx()],
+            &module.types,
+            &llvm_constants,
+        );
+    }
+
+    llvm_constants
+}
+
+pub(crate) fn generate_dynamic_constant_strings(module: &Module) -> Vec<String> {
+    // Render dynamic constants into LLVM IR.
+    let mut llvm_dynamic_constants = vec!["".to_string(); module.dynamic_constants.len()];
+    for id in (0..module.dynamic_constants.len()).map(DynamicConstantID::new) {
+        match &module.dynamic_constants[id.idx()] {
+            DynamicConstant::Constant(val) => llvm_dynamic_constants[id.idx()] = format!("{}", val),
+            DynamicConstant::Parameter(num) => {
+                llvm_dynamic_constants[id.idx()] = format!("%dc{}", num)
+            }
+        }
+    }
+
+    llvm_dynamic_constants
+}
+
+fn render_zero_constant(cons_id: ConstantID, ty: &Type) -> String {
+    // Extra logic for zero constants, since arrays are set to 0 by the runtime.
+    match ty {
+        Type::Array(_, _) => format!("%arr.{}", cons_id.idx()),
+        Type::Summation(_) => todo!(),
+        _ => "zeroinitializer".to_string(),
+    }
+}
